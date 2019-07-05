5dd632cf2147cbba1e69d9ff32526cff8cd7b8e3
Lucas Gabriel Dutra de Souza <lucasdutra@win.ufu.br>
2019-07-05 19:36:24 -0300

    projeto code blocks com arquivo externo

---
+++ Trabalho_Comp2/externo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float soma(float a, float b){
+    return a + b;
+}
--- Trabalho_Comp2/main.c
+++ Trabalho_Comp2/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "externo.h"
 int main()
 {
-    printf("Hello world!\n");
+    float a = 1.0, b = 2.0, saida = 0;
+
+    saida = soma(a,b);
+    printf("%f",saida);
     return 0;
 }
